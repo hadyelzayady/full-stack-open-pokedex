9ccfa478e9e8b9b1efcc58346c692e15b48e66ab
HadyElzayady <hadyelzayady@yahoo.com>
2021-03-25 12:41:14 +0200

    health chec

--- app.js
+++ app.js
@@ -5,9 +5,20 @@ const app = express()
 // eslint-disable-next-line no-undef
 const PORT = process.env.PORT || 5000
 
+app.get('/health', (req, res) => {
+  res.send('ok')
+})
+
+
+app.get('/version', (req, res) => {
+  res.send('1')
+})
+
 app.use(express.static('dist'))
+
 
 app.listen(PORT, () => {
   // eslint-disable-next-line no-console
   console.log('server started on port 5000')
 })
+
